952f7ca34573ca3bb07f688ea37631c7fdc1ac4f
MilaSong <songailaite.milita@gmail.com>
2023-02-20 19:39:56 +0200

    feat: created an API for the deep learning model

---
+++ makefile
@@ -0,0 +1,13 @@
+PYTHON=./.venv/bin/python
+PIP=./.venv/bin/pip
+
+create-virtualenv: 
+	python3 -m venv .venv
+	${PIP} install --upgrade pip
+	${PIP} install -r requirements.txt 
+
+run-tests:
+	${PYTHON} -m pytest 
+
+setup-local-api:
+	uvicorn src.api:app --reload
---
+++ src/predict.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from lime.lime_text import LimeTextExplainer
+
+class_names = ["fake", "real"]
+model = AutoModelForSequenceClassification.from_pretrained("ghanashyamvtatti/roberta-fake-news")
+tokenizer = AutoTokenizer.from_pretrained("ghanashyamvtatti/roberta-fake-news")
+
+def predictor(texts):
+    outputs = model(**tokenizer(texts, return_tensors="pt", padding=True))
+    tensor_logits = outputs[0]
+    probas = F.softmax(tensor_logits).detach().numpy()
+    return probas
+
+
+def get_explanation(text):
+    explainer = LimeTextExplainer(class_names=class_names)
+    exp = explainer.explain_instance(text, predictor, num_features=10, num_samples=20)
+    fig = exp.as_pyplot_figure()
+    plt.show()
