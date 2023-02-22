cb5d8c9a81635538da81012733fa795886903bc2
MilaSong <songailaite.milita@gmail.com>
2023-02-22 17:58:10 +0200

    fix: fixed docker-compose file to cache models

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-text_dict = {"article_text": "Donald Trump Sends Out Embarrassing New Years Eve Message; This is Disturbing,Donald Trump just couldn t wish all Americans a Happy New Year and leave it at that"}
+text_dict = {"article_text": "The apple is"}
 #text_dict = {"message_text": "i think some are actual russian bots to antagonize other nations citizens against ukraine to support russia",
 #             "threshold": 0.88}
 #response = requests.post('http://127.0.0.1:22222/match_fake/', json=text_dict)
